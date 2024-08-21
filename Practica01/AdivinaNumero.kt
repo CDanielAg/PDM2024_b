03f82ab818d535dc52ba47e4e208a27d056d9708
Carlos Daniel Aguilar Chirinos <caguilarc@ulasalle.edu.pe>
2024-08-21 17:51:45 -0500

    Agregando comentarios

--- Practica01/AdivinaNumero.kt
+++ Practica01/AdivinaNumero.kt
@@ -1,12 +1,12 @@
 fun main() {
-    val numero = (1..30).random()
+    val numero = (1..30).random() //variable con un numero entero random del 1 al 30
     println("Intenta adivinar el número(1-30):")
-    var respuesta:Int? = null
+    var respuesta:Int? = null    //Se crea una variable y esta predefinida en null
     do{
-        respuesta = readln().toInt()
+        respuesta = readln().toInt() //Se lee el imput por el teclado y el valor se transforma en entero
         if (respuesta == null || respuesta !in 1..30) {
             println("Por favor, ingresa un número válido entre 1 y 30.")
-            continue
+            continue //Se utiliza continue para saltar a la siguiente iteración del while
         }
         else if (respuesta > numero) println("El número es más pequeño.")
         else if (respuesta < numero) println("El número es más grande")
