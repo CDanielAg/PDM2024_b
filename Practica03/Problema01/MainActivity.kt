1d87c8fb94486030b9cfeeb2b594523eef365797
Carlos Daniel Aguilar Chirinos <caguilarc@ulasalle.edu.pe>
2024-09-15 23:54:16 -0500

    Agregando el boton siguiente junto al cambio de actividad

--- Practica03/Problema01/MainActivity.kt
+++ Practica03/Problema01/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.interaccinconimgenes
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,20 +17,27 @@ class MainActivity : AppCompatActivity() {
 
         val spinner: Spinner = findViewById(R.id.spinner_img)
         val items = listOf("Imagen 1", "Imagen 2", "Imagen 3")
+        val btn: Button = findViewById(R.id.Siguiente)
 
+        // Configuración del adaptador para el Spinner
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         spinner.adapter = adapter
+
+        var selectedItem = ""
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedItem = items[position]
+                selectedItem = items[position]
                 Toast.makeText(this@MainActivity, "$selectedItem", Toast.LENGTH_SHORT).show()
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        //boton para pasar de interfaz y pasar el item seleccionado a la siguiente interfaz
+        btn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("selectedItem", selectedItem)
+            startActivity(intent)
         }
     }
 }
